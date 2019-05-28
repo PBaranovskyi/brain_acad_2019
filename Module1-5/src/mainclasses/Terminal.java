84a4aae758ebc5b55b32e50db11257e90531adb0
Pavlo Baranovskyi <baranovskiy.p.p@gmail.com>
2019-05-28 19:46:29 +0300

    first commit

---
+++ Module1-5/src/mainclasses/Terminal.java
@@ -0,0 +1,40 @@
+package mainclasses;
+
+/**
+ * Created by citsym on 5/17/19.
+ */
+class Terminal1 {
+
+    static public void main(String[] args) {
+
+        char symbolh = 'h';
+        char symbolH = 'H';
+
+        System.out.println(symbolh > symbolH );
+
+
+        String textValue = "text".toUpperCase();
+
+        System.out.println(textValue);
+
+        byte byteType = 127;
+
+        byte byteType2 = ++byteType;
+
+        System.out.println(byteType2);
+
+
+        int marks = 8;
+
+        ++marks;
+
+        System.out.println("ffff" + 'f');
+
+
+    }
+
+}
+
+class A {
+
+}
