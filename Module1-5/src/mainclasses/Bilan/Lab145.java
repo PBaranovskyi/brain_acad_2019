a852108367f0c873e4c650517735c7ce9eb0241e
mainmax82 <golosei@gmail.com>
2019-05-28 20:14:17 +0300

    Lab 1_4_1 to 1_5_3

---
+++ Module1-5/src/mainclasses/Bilan/Lab145.java
@@ -0,0 +1,12 @@
+package mainclasses.Bilan;
+
+public class Lab145 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println(a<<2);
+        System.out.println(a>>2);
+        System.out.println(a>>>1);
+    }
+}
+
---
+++ Module1-5/src/mainclasses/Bilan/Main.java
@@ -0,0 +1,32 @@
+package mainclasses.Bilan;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x > 0) {
+            System.out.println("Введите число от 1 до 3 и узнайте имя своего кота");
+            Scanner k = new Scanner(System.in);
+            int number1 = k.nextInt();
+            String name1 = "Васька";
+            String name2 = "Мурчик";
+            String name3 = "Пушок";
+            if (number1 == 1) {
+                System.out.println("Имя вашего кота: " + name1);
+            }
+            if (number1 == 2) {
+                System.out.println("Имя вашего кота:  " + name2);
+            }
+            if (number1 == 3) {
+                System.out.println("Имя вашего кота:  " + name3);
+            }
+            if (number1 > 3) {
+                System.out.println("Все-таки  "  +number1 +  "   не попадает в предложенные числа");
+            }
+            if (number1 == 0) {
+                System.out.println("Вы что не любите котов?");
+            }
+        }
+    }
+}
