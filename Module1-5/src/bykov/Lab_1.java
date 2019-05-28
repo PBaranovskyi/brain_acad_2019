dda624952611f1a0c47b1d286a2dc28ff79d33b9
bukosjkee <denver1994@gmail.com>
2019-05-28 20:54:57 +0300

    Lab 1

---
+++ Module1-5/src/bykov/Lab_1.java
@@ -0,0 +1,12 @@
+package bykov;
+
+public class Lab_1 {
+    public static void main(String[] args) {
+        System.out.println("***  *    *   *  *");
+        System.out.println("  *  **   *  *  **");
+        System.out.println("  *  * *  *  * *  *");
+        System.out.println("  *  **** * *  ****");
+        System.out.println("***  *  *  *   *   *");
+
+    }
+}
