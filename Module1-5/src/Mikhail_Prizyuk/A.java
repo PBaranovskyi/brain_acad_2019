4903a0edafb7ad1a727a828c330bfa867c563c39
zigmond_f <mikhail.prizyuk@gmail.com>
2019-05-28 21:13:34 +0300

    new

---
+++ Module1-5/src/Mikhail_Prizyuk/A.java
@@ -0,0 +1,11 @@
+package Mikhail_Prizyuk;
+
+public class A {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 9;
+        double c = 5.11;
+        double x = a + c;
+        System.out.println(x);
+    }
+}
