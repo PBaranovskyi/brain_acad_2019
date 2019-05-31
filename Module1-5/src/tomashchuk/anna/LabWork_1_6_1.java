e8d1923c6e1fc70c4161b2f01b36df3a99c9e33e
atomashchuk <ayanovych@gmail.com>
2019-05-31 20:07:41 +0300

    New lab

---
+++ Module1-5/src/tomashchuk/anna/LabWork_1_6_1.java
@@ -0,0 +1,10 @@
+package tomashchuk.anna;
+
+public class LabWork_1_6_1 {
+    public static void main(String[] args) {
+        for(int i=2;i<=30;i+=2){
+            System.out.println(i);
+        }
+    }
+}
+
