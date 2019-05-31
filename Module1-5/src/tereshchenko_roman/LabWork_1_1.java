274804f7c341df4392a8e3c476e57aa7657451a8
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-05-31 18:22:44 +0300

    My lab works 1_5

---
+++ Module1-5/src/tereshchenko_roman/LabWork_1_1.java
@@ -0,0 +1,11 @@
+package com.brainacad.lesson1;
+
+public class LabWork_1_1 {
+    public static void main(String[] args) {
+        System.out.println(" * * *       *     *       *    *  ");
+        System.out.println("      *     * *     *     *    * * ");
+        System.out.println("      *    *   *     *   *    *   *");
+        System.out.println("*      *    ****      * *      ****");
+        System.out.println(" * * *     *   *       *      *   *");
+    }
+}
---
+++ Module1-5/src/tereshchenko_roman/LabWork_1_4_2.java
@@ -0,0 +1,13 @@
+package com.brainacad.lesson1;
+
+public class LabWork_1_4_2 {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 7;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }}
