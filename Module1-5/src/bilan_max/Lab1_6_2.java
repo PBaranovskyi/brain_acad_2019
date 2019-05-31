3fd49e564e499719321ad06d1a4e4d9961a12dfa
mainmax82 <golosei@gmail.com>
2019-05-31 19:39:21 +0300

    Lab 1_6_1 to 1_6_3

---
+++ Module1-5/src/bilan_max/Lab1_6_2.java
@@ -0,0 +1,22 @@
+package bilan_max;
+
+public class Lab1_6_2 {
+    public static void main(String[] args) {
+        int[] m = new int[]{10, 21, 5, 22, 9, 29, 25, 22, 11, 14, 8, 14};
+        int max = m[0];
+        int min = m[0];
+        double average = 0;
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            if (m[i] > max) {
+                max = m[i];
+            }
+            if (m[i] < min) {
+                min = m[i];
+            }
+            sum += m[i];
+            average = sum / m.length;
+        }
+        System.out.println("Минимальное значение" + min + "Максимальное значение " + max + " Среднеарифмитичекое значение " + average);
+    }
+}
---
+++ Module1-5/src/bilan_max/Lab1_6_3.java
@@ -0,0 +1,18 @@
+package bilan_max;
+
+public class Lab1_6_3 {
+    public static void main(String[] args) {
+        int[][] arr = new int[4][4];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (i+1)*(j+1);
+            }
+        }
+        for (int [] anArr : arr) {
+            for (int anAnArr : anArr) {
+                System.out.print(anAnArr + "|");
+            }
+            System.out.println( );
+        }
+    }
+}
