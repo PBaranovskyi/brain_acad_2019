dbfd20ed453ba72d91ebe5f53e16f8d934590ab5
Pavlo Baranovskyi <baranovskiy.p.p@gmail.com>
2019-05-31 21:04:55 +0300

    some work arrays

--- Module1-5/src/tereshchenko_roman/LabWork_1_6_4.java
+++ Module1-5/src/tereshchenko_roman/LabWork_1_6_4.java
@@ -7,7 +7,7 @@ public class LabWork_1_6_4 {
         int[] myArray = {2, 5, 12, 65, 38, 13, 29, 698};
         Arrays.sort(myArray);
         System.out.println(Arrays.toString(myArray));
-       int key = 13;
+        int key = 13;
         System.out.println(Arrays.binarySearch(myArray, key));
     }
 }
--- Module1-5/src/tomashchuk/anna/LabWork_1_6_1.java
+++ Module1-5/src/tomashchuk/anna/LabWork_1_6_1.java
@@ -2,8 +2,18 @@ package tomashchuk.anna;
 
 public class LabWork_1_6_1 {
     public static void main(String[] args) {
-        for(int i=2;i<=30;i+=2){
-            System.out.println(i);
+//        for (int i = 2; i <= 30; i += 2) {
+//            System.out.println(i);
+//        }
+
+        final int MAX_VALUE = 30;
+        int counter = 0;
+        int[] arr = new int[MAX_VALUE / 2];
+        for (int i = 1; i <= MAX_VALUE; i++) {
+            if (i % 2 == 0) {
+                arr[counter++] = i;
+                System.out.println(i);
+            }
         }
     }
 }
