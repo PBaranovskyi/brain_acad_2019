08afdfdfdd7ea0c72766817fd1a885a7bd6b1be6
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-06-03 10:16:10 +0300

    My lab works 1_5

---
+++ Module1-5/src/tereshchenko_roman/arrays_Add_Home_Works/Add_Home_Work_24.java
@@ -0,0 +1,17 @@
+package com.brainacad.arrays.arrays_Add_Home_Works;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Add_Home_Work_24 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+
+        Random rand = new Random();
+        for (int a = 0; a < 10; a++) {
+            arr[a] = rand.nextInt(100);
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
