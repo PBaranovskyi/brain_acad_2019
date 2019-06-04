d16e2e1722787b83ce04c4c1d94c4f1de8f66496
bukosjkee <denver1994@gmail.com>
2019-06-04 09:56:35 +0300

    Labs from 1.4.1 to 1.5.1

---
+++ Module1-5/src/bykov/Lab1_4_1.java
@@ -0,0 +1,22 @@
+public class Lab1_4_1 {
+    public static void main(String[] args) {
+        byte a = 31;
+        short b = -29_753;
+        int c = 2147325623;
+        long d = 1241245789239499l;
+        float e = 1.22E+34F;
+        double f = 1.79E+308;
+        char g = 'L';
+        boolean h = false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+
+    }
+}
