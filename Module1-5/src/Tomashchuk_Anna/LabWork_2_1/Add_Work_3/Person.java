6c951c0c519ff830bc3764558a5bdc3a6b4729d7
atomashchuk <ayanovych@gmail.com>
2019-06-13 15:35:43 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_1/Add_Work_3/Person.java
@@ -0,0 +1,21 @@
+package LabWork_2_1.Add_Work_3;
+
+public class Person {
+    public String name;
+    public int age;
+    public String gender;
+    public int yearOfBirth;
+
+    void setName(String name) {this.name=name;}
+    String getName() {return name;}
+
+    void setAge(int age) {this.age=age;}
+    int getAge() {return age;}
+
+    void setGender(String gender) {this.gender=gender;}
+    String getGender() {return gender;}
+
+    void setYearOfBirth(int yearOfBirth) {this.yearOfBirth=yearOfBirth;}
+    int getYearOfBirth() {return yearOfBirth;}
+
+}
---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_1/Add_Work_3/PersonMain.java
@@ -0,0 +1,18 @@
+package LabWork_2_1.Add_Work_3;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        Person one = new Person();
+
+        one.setName("Vasya");
+        one.setAge(12);
+        one.setGender("M");
+        one.setYearOfBirth(1981);
+
+        System.out.println("Name of Person: " + one.getName());
+        System.out.println("Age: " + one.getAge());
+        System.out.println("Gender: " + one.getGender());
+        System.out.println("YearOfBirth: " + one.getYearOfBirth());
+
+    }
+}
