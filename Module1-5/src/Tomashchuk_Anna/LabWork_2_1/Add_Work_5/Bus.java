4a8e7edfe4d127ea647469df1d3427150058985f
atomashchuk <ayanovych@gmail.com>
2019-06-14 13:45:04 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_1/Add_Work_5/Bus.java
@@ -0,0 +1,13 @@
+package LabWork_2_1.Add_Work_5;
+
+public class Bus {
+    private int seats;
+
+    // public Method
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getSeats() {return this.seats;}
+
+}
