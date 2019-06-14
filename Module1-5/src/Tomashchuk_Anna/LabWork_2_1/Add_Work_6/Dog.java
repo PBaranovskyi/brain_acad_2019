3f3012310319ccf2f1b06065a4426046ad329b77
atomashchuk <ayanovych@gmail.com>
2019-06-14 16:57:21 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_1/Add_Work_6/Dog.java
@@ -0,0 +1,12 @@
+package LabWork_2_1.Add_Work_6;
+
+public class Dog {
+    private String name;
+    private int age;
+
+    public void setName(String name) { this.name = name; }
+    public String getName() {return name;}
+
+    public void setAge(int age) {this.age=age;}
+    public int getAge() {return age;}
+}
---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_1/Add_Work_6/DogMain.java
@@ -0,0 +1,14 @@
+package LabWork_2_1.Add_Work_6;
+
+public class DogMain {
+    public static void main(String[] args) {
+        Dog obj1 = new Dog();
+
+        obj1.setName("Тузик_6");
+        obj1.setAge(4);
+
+        System.out.println("NameOfDog: " + obj1.getName());
+        System.out.println("Age: " + obj1.getAge());
+
+    }
+}
