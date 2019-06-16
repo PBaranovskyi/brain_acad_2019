aa99399be77b66c95576a5f0e0fa0ba64eace23a
atomashchuk <ayanovych@gmail.com>
2019-06-16 18:01:49 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_2/Sample_Multiplication/MainMatrix.java
@@ -0,0 +1,22 @@
+package LabWork_2_2.Sample_Multiplication;
+
+import java.util.Arrays;
+
+public class MainMatrix {
+    public static void main(String[] args) {
+        int[][] arr1 = {{1, 2, 3}, {4, 3, 2}, {5, 1, 3}};
+        int[][] arr2 = {{2, 3, 1}, {1, 1, 4}, {1, 2, 1}};
+
+        Matrix matrix=new Matrix();
+        Matrix matrix2=new Matrix();
+
+        matrix.setMult(arr1);
+        matrix2.setMult(arr2);
+//  multiplication Можно назвать по-другому это просто переменная
+        int[][] mult=matrix.multiplication(matrix2);
+        System.out.println(Arrays.deepToString(mult));
+
+
+
+    }
+}
---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_2/Sample_Multiplication/Matrix.java
@@ -0,0 +1,29 @@
+package LabWork_2_2.Sample_Multiplication;
+
+public class Matrix {
+
+    public int[][] getMult() {
+        return multiply;
+    }
+
+    private int[][] multiply;
+
+    //  multiplication Можно назвать по-другому это просто переменная
+    public int[][] multiplication(Matrix obj) {
+        int[][] incomeMatrix = obj.getMult();
+
+       int[][] mult  = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                mult[i][j] = multiply[i][j] * incomeMatrix[i][j];
+            }
+       }
+       return mult;
+    }
+
+
+    public void setMult(int[][] multiply) {
+        this.multiply = multiply;}
+
+
+}
