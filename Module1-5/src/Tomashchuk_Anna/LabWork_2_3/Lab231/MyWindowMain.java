703281bae680ba4fdd3de2c78c5bceea455a9225
atomashchuk <ayanovych@gmail.com>
2019-06-18 18:36:10 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_3/Lab231/MyWindowMain.java
@@ -0,0 +1,13 @@
+package LabWork_2_3.Lab231;
+
+public class MyWindowMain {
+    public static void main(String[] args) {
+        MyWindow myWindow=new MyWindow("White", 3.2, 0.9);
+        double win;
+
+        //win=myWindow.printFields(5,6);
+
+
+
+    }
+}
