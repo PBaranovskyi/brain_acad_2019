e1f23c8465ff02ca3b7123c90319df66e6eb7788
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-06-18 17:15:26 +0300

    My lab works 1_5

---
+++ Module1-5/src/tereshchenko_roman/constructors/MainMyWindow.java
@@ -0,0 +1,33 @@
+package com.brainacad.constructors;
+
+public class MainMyWindow {
+    public static void main(String[] args) {
+        MyWindow window1 = new MyWindow(32.15, 33.18, 3, "white", true);
+        MyWindow window2 = new MyWindow(33.15, 37.18, 2, "white", false);
+        MyWindow window3 = new MyWindow(34.15, 38.18, 3, "white", true);
+        MyWindow window4 = new MyWindow(35.15, 39.18, 2, "white", false);
+        MyWindow window5 = new MyWindow(36.15, 40.18, 3, "white", true);
+
+
+//        public void printFields () { ???
+
+            MyWindow[] arr = new MyWindow[5];
+
+            arr[0] = window1;
+            arr[1] = window2;
+            arr[2] = window3;
+            arr[3] = window4;
+            arr[4] = window5;
+
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Width: " + arr[i].getWidth());
+                System.out.println("Height: " + arr[i].getHeight());
+                System.out.println("Number of glasses: " + arr[i].getNumberOfGlass());
+                System.out.println("Color: " + arr[i].getColor());
+                System.out.println("Opens: " + arr[i].getIsOpen());
+                System.out.println();
+            }
+    }
+
+        }
+
---
+++ Module1-5/src/tereshchenko_roman/constructors/MyWindow.java
@@ -0,0 +1,61 @@
+package com.brainacad.constructors;
+
+public class MyWindow {
+
+    double width;
+    double height;
+    int numberOfGlass;
+    String color;
+    boolean isOpen;
+
+
+    public MyWindow(double width, double height, int numberOfGlass, String color, boolean isOpen) {
+        this.width = width;
+        this.height = height;
+        this.numberOfGlass = numberOfGlass;
+        this.color = color;
+        this.isOpen = isOpen;
+    }
+
+
+//     Lab Work 2_3_2
+    public MyWindow(){
+        this.width = width;
+        this.height = height;
+    }
+
+//    Lab Work 2_3_3
+    public MyWindow(double width, double height){
+    }
+
+    public MyWindow(double width, double height, int numberOfGlass){
+           }
+
+//    Lab Work 2_3_4
+//    public MyWindow(double width, double height, int numberOfGlass){
+//        this.width = width;
+//        this.height = height;
+//        this.numberOfGlass = numberOfGlass;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getNumberOfGlass() {
+        return numberOfGlass;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean getIsOpen() {
+        return isOpen;
+    }
+}
+
+
