97c4c56cc266a38e2f8e51a78dc96b4a6a1dd88f
atomashchuk <ayanovych@gmail.com>
2019-06-18 16:51:20 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_3/Construct_Vihicle/VehConsDemo.java
@@ -0,0 +1,20 @@
+package LabWork_2_3.Construct_Vihicle;
+
+public class VehConsDemo {
+    public static void main(String[] args) {
+        //завершение создания объектов
+       Vihicle minivan=new Vihicle(7,16,21);
+       Vihicle sportcar=new Vihicle(2,14,12);
+       double gallons;
+       int dist=252;
+
+       gallons=minivan.fuelneeded(dist);
+        System.out.println("Для преодоления "+dist+" миль мини-фургону требуется "+
+                + gallons+" галлонов топлива");
+
+        gallons=sportcar.fuelneeded(dist);
+        System.out.println("Для преодоления "+dist+" спорт-авто "+
+                + gallons+" галлонов топлива");
+
+    }
+}
---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_3/Construct_without_Param/MyClass.java
@@ -0,0 +1,11 @@
+package LabWork_2_3.Construct_without_Param;
+
+public class MyClass {
+    int x;
+
+    //Конструктор класса MyClass
+    // Имя конструктора совпадает с именем класса
+    MyClass() {
+        x=15;
+    }
+}
