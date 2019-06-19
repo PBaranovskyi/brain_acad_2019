cf5420e70b63637e2e0feb8dcaf071d20c61436c
atomashchuk <ayanovych@gmail.com>
2019-06-19 13:40:21 +0300

    New lab

--- Module1-5/src/Tomashchuk_Anna/LabWork_2_1/Test_2_1/B.java
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_1/Test_2_1/B.java
@@ -1,16 +1,14 @@
 package Tomashchuk_Anna.LabWork_2_1.Test_2_1;
 
 public class B {
-    private int x1;
-    public void setX(int x){
-        x1=x;
+    private int x1; // x1=0 по дефолту , так как значение не задано
+
+    public void setX(int x){ //b.setX(5) - из метода main, згачит сюда в x  - setX(int x) прописываем 5, тоесть переменная int x=5
+        x1=x; // x1 было 0, теперь x1 переприсвоили значение 5, x1=5
     }
-    public int getX(){
-        return x1;
+    public int getX(){ // из метода main System.out.println(b.getX());
+        return x1; // возвращаем x1=5
     }
-    public static void main(String[] args){
-        B b=new B();
-        b.setX(5);
-        System.out.println(b.getX());
-    }
+
+
 }
---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_1/Test_2_1/B_Main.java
@@ -0,0 +1,9 @@
+package Tomashchuk_Anna.LabWork_2_1.Test_2_1;
+
+public class B_Main {
+    public static void main(String[] args){
+        B b=new B();
+        b.setX(5);
+        System.out.println(b.getX());
+    }
+}
