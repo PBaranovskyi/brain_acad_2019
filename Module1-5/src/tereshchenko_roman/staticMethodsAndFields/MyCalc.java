0bc9a01fce9ea6a6a626c868ee988bb2d2a285bd
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-06-23 13:40:49 +0300

    My lab works 1_5

---
+++ Module1-5/src/tereshchenko_roman/staticMethodsAndFields/MyCalc.java
@@ -0,0 +1,17 @@
+package tereshchenko_roman.staticMethodsAndFields;
+
+public class MyCalc {
+
+    private static double Pi;
+
+    public static double calcPi(int n) {
+
+        Pi = 4.0;
+        int v = 0;
+        for (int i = 0; i <= n; i++) {
+            Pi = Pi - 4.00 / (3 + v) + 4.00 / (5 + v);
+            v = v + 4;
+        }
+        return Pi;
+    }
+}
---
+++ Module1-5/src/tereshchenko_roman/staticMethodsAndFields/MyCalcMain.java
@@ -0,0 +1,24 @@
+package tereshchenko_roman.staticMethodsAndFields;
+
+
+import java.util.Scanner;
+
+public class MyCalcMain {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Введите параметр N - уровень точности вычисления числа π - от 0 до 999999999.");
+        Scanner scan = new Scanner(System.in);
+        int n;
+        n = scan.nextInt();
+
+
+        System.out.print("Число π на этом уровне точности вычисления равно ");
+        System.out.print(MyCalc.calcPi(n));
+        System.out.print(".");
+        //        System.out.printf("%.7f", MyCalc.calcPi(n));
+
+    }
+}
