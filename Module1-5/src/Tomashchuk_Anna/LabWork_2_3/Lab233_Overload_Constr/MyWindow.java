9998a88bb526fdad83a8913ebaf2bdcc9ba993f4
atomashchuk <ayanovych@gmail.com>
2019-06-24 18:57:13 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_3/Lab233_Overload_Constr/MyWindow.java
@@ -0,0 +1,70 @@
+package Tomashchuk_Anna.LabWork_2_3.Lab233_Overload_Constr;
+
+public class MyWindow {
+    public double width;
+    public double height;
+    public int numberOfGlass;
+    public String color;
+    public boolean open;
+
+    public void setNumberOfGlass(int numberOfGlass){
+        if (numberOfGlass>3) {
+            System.out.println("Not allowed to create a window with numberOfGlass: "+ numberOfGlass);
+        }
+        else{
+            this.numberOfGlass=numberOfGlass;
+       }
+    }
+
+   public int getNumberOfGlass() {return numberOfGlass;}
+
+//////////////////////////// overloaded constructors
+    // Это конструктор класса MyWindow
+
+    // Конструктор без параметров
+    public MyWindow() {
+        color="White";
+        width=2.7;
+        height=3.0;
+    }
+
+    public MyWindow( double wid, double hei ) {
+        width=wid;
+        height=hei;
+    }
+
+    public MyWindow( double wid, double hei , int glass) {
+        width=wid;
+        height=hei;
+        numberOfGlass=glass;
+    }
+
+    public MyWindow( String col,double wid, double hei ) {
+        color=col;
+        width=wid;
+        height=hei;
+    }
+////////////////////////////
+    // Определение размера окна
+    double size() {
+        return width*height;
+    }
+
+   // public void setOpen(boolean open) {this.open = open;}
+   // public boolean isOpen() {     return open;}
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() { return color; }
+
+    // Метод printFields
+    public String printFields(String color, double width, double height, int numberOfGlass, boolean open) {
+        System.out.println("Color: "+color+", Width: "+width+", Heigth: "+height+", NumberOfGlass: "+numberOfGlass+
+                ", IsOpen: "+open);
+        return color+width+height+numberOfGlass+open;
+    }
+
+
+}
---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_3/Lab233_Overload_Constr/MyWindowMain.java
@@ -0,0 +1,25 @@
+package Tomashchuk_Anna.LabWork_2_3.Lab233_Overload_Constr;
+
+public class  MyWindowMain {
+    public static void main(String[] args) {
+        MyWindow myWindow=new MyWindow("White", 3.2, 0.9);
+        MyWindow myWindow2=new MyWindow(2.4,4.0,7);
+
+        System.out.println(myWindow.getColor());
+        System.out.println(myWindow.color);
+        System.out.println(myWindow.width);
+        System.out.println(myWindow.height);
+
+        //Это мы вызываем Конструктор
+        System.out.println("Constructor : "+myWindow.color+" "+myWindow.width+" "+myWindow.height);
+
+        myWindow.numberOfGlass=2; //-Почему не выводит ? Not allowed to create a window with numberOfGlass
+        System.out.println("NumberOfGlass: "+myWindow.getNumberOfGlass());
+
+        myWindow.printFields("Brown",2.8,4.2,6,true);
+
+        System.out.println("Constructor_2: width:"+myWindow2.width+", heigth: "+myWindow2.height+", Window: "+ myWindow2.numberOfGlass);
+
+
+    }
+}
