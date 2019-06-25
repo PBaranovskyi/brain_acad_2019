d5b5b70a297727a247ed9377620f6bb4e98edcf3
MykhailoMike <mkaloshyn@gmail.com>
2019-06-25 19:14:50 +0300

    My lab 2_4

---
+++ Module1-5/src/mykhailo_kaloshyn/mainacad/lab2_4/Main.java
@@ -0,0 +1,26 @@
+package mykhailo_kaloshyn.mainacad.lab2_4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*double r = 3;
+        int[] arr1 = {5, 7, 9, -2, 0, 11};
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println("Max: " + MyMath.findMax(arr1));
+        System.out.println("Min: " + MyMath.findMin(arr1));
+
+        System.out.println("The square of circle with the radius " + r + " is equal: " + MyMath.areaOfCircle(r));
+
+        Employee emp1 = new Employee("Ivan", "Ivanenko", "driver", 244516);
+        Employee emp2 = new Employee("Petrov", "Egor", "fireman", 366090);
+        Employee emp3 = new Employee("Geda", "Marina", "assistant", 519087);
+
+        System.out.println("Number of employess is: " + emp1.getNumberOfEmployees());
+
+        System.out.println("Number Pi is equal to: " + MyCalc.calcPi(5));*/
+
+        MyPyramid.printPyramid(11);
+
+    }
+}
