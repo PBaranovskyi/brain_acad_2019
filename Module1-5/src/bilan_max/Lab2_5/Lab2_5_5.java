83570fc0f8de2ff9256dae1912899e5b73d4bf5b
mainmax82 <golosei@gmail.com>
2019-07-02 13:20:14 +0300

    Lab2_5

---
+++ Module1-5/src/bilan_max/Lab2_5/Lab2_5_5.java
@@ -0,0 +1,18 @@
+package bilan_max.Lab2_5;
+
+public class Lab2_5_5 {
+    public static void main(String[] args) {
+
+        InitTest InitTest1 = new InitTest();
+        InitTest InitTest2 = new InitTest();
+        InitTest InitTest3 = new InitTest();
+        InitTest InitTest4 = new InitTest();
+        InitTest InitTest5 = new InitTest();
+
+        System.out.println("ID of InitTest1 is: " + InitTest1.getId());
+        System.out.println("ID of InitTest2 is: " + InitTest2.getId());
+        System.out.println("ID of InitTest3 is: " + InitTest3.getId());
+        System.out.println("ID of InitTest4 is: " + InitTest4.getId());
+        System.out.println("ID of InitTest5 is: " + InitTest5.getId());
+    }
+}
