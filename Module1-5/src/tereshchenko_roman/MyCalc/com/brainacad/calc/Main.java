d951c72fa08293dc1ed7c5d9b897a3a720f61d8a
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-07-05 19:03:39 +0300

    My lab works 1_5

---
+++ Module1-5/src/tereshchenko_roman/MyCalc/com/brainacad/calc/Main.java
@@ -0,0 +1,13 @@
+package tereshchenko_roman.MyCalc.com.brainacad.calc;
+
+import tereshchenko_roman.MyCalc.com.brainacad.shapes.Triangle;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle triangle1 =  new Triangle(5,7,9);
+
+        System.out.println(triangle1.getArea());
+
+    }
+}
---
+++ Module1-5/src/tereshchenko_roman/MyCalc/com/brainacad/shapes/Triangle.java
@@ -0,0 +1,21 @@
+package tereshchenko_roman.MyCalc.com.brainacad.shapes;
+import static java.lang.Math.sqrt;
+
+public class Triangle {
+    double a = 1.0;
+    double b = 1.0;
+    double c = 1.0;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getArea (){
+        double p = (a + b + c)/2;
+        double area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        return area;
+    }
+
+}
