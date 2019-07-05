9f7dd4dc9e4c77a7f425ee1f77835426db6e087f
atomashchuk <ayanovych@gmail.com>
2019-07-05 19:51:15 +0300

    New lab

--- Module1-5/src/Tomashchuk_Anna/LabWork_2_6_Packages/com/brainacad/calc/Main.java
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_6_Packages/com/brainacad/calc/Main.java
@@ -1,7 +1,10 @@
 package Tomashchuk_Anna.LabWork_2_6_Packages.com.brainacad.calc;
 
+import Tomashchuk_Anna.LabWork_2_6_Packages.com.brainacad.shapes.Triangle;
+
 public class Main {
     public static void main(String[] args) {
-
+        Triangle triangle = new Triangle(4, 7, 3);
+        System.out.println(triangle.getArea());
     }
 }
--- Module1-5/src/Tomashchuk_Anna/LabWork_2_6_Packages/com/brainacad/shapes/Triangle.java
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_6_Packages/com/brainacad/shapes/Triangle.java
@@ -1,7 +1,19 @@
 package Tomashchuk_Anna.LabWork_2_6_Packages.com.brainacad.shapes;
 
 public class Triangle {
-    private double a=1.0;
-    private double b=1.0;
-    private double c=1.0;
+    private double a = 1.0;
+    private double b = 1.0;
+    private double c = 1.0;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getArea() {
+        double p = (a + b + c) / 2;
+        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return area;
+    }
 }
