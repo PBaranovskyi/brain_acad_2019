1cb24e16d15a7bd0b07b41c79333cf91d5c48bd4
atomashchuk <ayanovych@gmail.com>
2019-07-11 18:27:02 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_7_Inheritance_Polymorphism/Course_2/SportCar.java
@@ -0,0 +1,8 @@
+package Tomashchuk_Anna.LabWork_2_7_Inheritance_Polymorphism.Course_2;
+
+public class SportCar extends Car {
+    @Override
+    public void testMethod(){
+        System.out.println("SportCar");
+    }
+}
