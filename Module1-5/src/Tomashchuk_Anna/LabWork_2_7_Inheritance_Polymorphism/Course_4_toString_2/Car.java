0a3c1b1c7ad9591ee62deb147a37e5e366cda624
atomashchuk <ayanovych@gmail.com>
2019-07-12 16:18:48 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_7_Inheritance_Polymorphism/Course_4_toString_2/Car.java
@@ -0,0 +1,8 @@
+package Tomashchuk_Anna.LabWork_2_7_Inheritance_Polymorphism.Course_4_toString_2;
+
+public class Car {
+    @Override
+    public String toString(){
+        return "This is Car";
+    }
+}
