9e64e6eca3529e2293dcc2f13ae615395327d372
MykhailoMike <mkaloshyn@gmail.com>
2019-07-16 18:33:21 +0300

    My inheritance HW1 has been deleted

--- Module1-5/src/mykhailo_kaloshyn/mainacad/myInheritance/Bear.java
+++
@@ -1,8 +0,0 @@
-package mykhailo_kaloshyn.mainacad.myInheritance;
-
-import mainclasses.inheritance2.Animal;
-public class Bear extends Animal {
-    public Bear(String name) {
-        super(name);
-    }
-}
--- Module1-5/src/mykhailo_kaloshyn/mainacad/myInheritance/Tiger.java
+++
@@ -1,8 +0,0 @@
-package mykhailo_kaloshyn.mainacad.myInheritance;
-
-public class Tiger extends mainclasses.inheritance2.Tiger {
-
-    public Tiger(String name) {
-        this.name = name;
-    }
-}
