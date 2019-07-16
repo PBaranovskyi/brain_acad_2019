d2824af06208f6d6748460977fc1b957dab6e2c0
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-07-16 18:46:27 +0300

    My lab works 1_5

--- Module1-5/src/tereshchenko_roman/inheritanceAndPolymorphism/Animal.java
+++ Module1-5/src/tereshchenko_roman/inheritanceAndPolymorphism/Animal.java
@@ -1,51 +1,94 @@
 package tereshchenko_roman.inheritanceAndPolymorphism;
 
 public class Animal {
-
     private String name;
+    private int age;
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getAge() {
+        return age;
+    }
+
+    public boolean equals(Animal animal) {
+        if (this.name == animal.name && this.age == animal.age) return true;
+        return false;
     }
 }
 
 class Tiger extends Animal {
-//    private String name = "Тигр";
-//
-//    public String getName() {
-//        return name;
-//    }
+    private String name;
+    private int age;
 
-    public String toString(){
-        return "Tiger";
+    public Tiger(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Animal tiger) {
+        if (this.name == name && this.age == age) return true;
+        return false;
     }
 }
 
 class Bear extends Animal {
-//    private String name = "Медведь";
-//
-//    public String getName() {
-//        return name;
-//    }
+    private String name;
+    private int age;
 
-    public String toString(){
-        return "Bear";
+    public Bear(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Animal bear) {
+        if (this.name == name && this.age == age) return true;
+        return false;
     }
 }
 
 class Goat extends Animal {
-//    private String name = "Коза";
-//
-//    public String getName() {
-//        return name;
-//    }
+    private String name;
+    private int age;
 
-    public String toString(){
-        return "Goat";
+    public Goat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Animal goat) {
+        if (this.name == name && this.age == age) return true;
+        return false;
     }
 }
+
+
 
--- Module1-5/src/tereshchenko_roman/inheritanceAndPolymorphism/Zoo.java
+++ Module1-5/src/tereshchenko_roman/inheritanceAndPolymorphism/Zoo.java
@@ -2,36 +2,86 @@ package tereshchenko_roman.inheritanceAndPolymorphism;
 
 public class Zoo {
 
-    public void getAnimals(Animal[] zooAnimals) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println(zooAnimals[i].getName());
-        }
-    }
+    // Определяем размер зоопарка:
+    public static int zooSize = 20;
 
-//    public void getAnimalNames(String[] zooAnimalsNames) {
-//        for (int i = 0; i < 3; i++) {
-//            System.out.println(zooAnimalsNames[i]);
+    // Создаём места для зверей:
+    static Animal[] zooResidents = new Animal[zooSize];
+
+//    // Делаем все места в зоопарке равными и пустыми (это нужно, чтобы сработал метод takeAnimals):
+//    static void createSpots(Animal[] arr) {
+//        for (Animal spot : zooResidents) {
+//            spot = null;
 //        }
 //    }
 
+    // Принимаем зверей по 3 за один раз(в следующие свободные места принимаем 3 новых зверей, делаем это один раз):
+    public void takeAnimals(Animal newResident1, Animal newResident2, Animal newResident3) {
+        for (int i = 0; i < zooSize - 2; i++) {
+            if (zooResidents[i] == null) {
+                zooResidents[i] = newResident1;
+                zooResidents[i + 1] = newResident2;
+                zooResidents[i + 2] = newResident3;
+                break;
+            }
+        }
+
+    }
+
+    //    Показываем список принятых зверей, при этом проверяем, чтобы каждый следующий зверь не был кем-то из предыдущих:
+    public void showAnimals(Animal[] arr) {
+//        for (Animal zooResident : arr) {// Без проверки зверя равен ли он предыдущему.
+//            if (zooResident != null) {
+//                System.out.println(zooResident.getName() + ", " + zooResident.getAge() + " years old");
+//            }
+        System.out.println(arr[0].getName() + ", " + arr[0].getAge() + " years old");// Данные по первому зверю берутся
+        // без проверки
+
+        // Проверка равен ли последующий зверь какому-то из предыдущих:
+        for (int m = 1; m < arr.length; m++) {// Берётся каждый зверь, начиная со второго, чтобы проверить, равен
+            // ли он кому-то из предыдущих.
+            for (int n = 0; n < m; n++) { //Берётся каждый предыдущий зверь, чтобы проверить, равен ли ему зверь m.
+                if (arr[m] != null) { // Эта проверка здесь ни при чём, она только для метода takeAnimals (чтобы не
+                    // выводить пустые места)
+                    if (arr[m].equals(arr[n])) {
+                        continue; // Не выводит ничего, если зверь равен одному из предыдущих
+                    }
+                    System.out.println(arr[m].getName() + ", " + arr[m].getAge() + " years old");// Выводит зверя, если
+                    // он не равен ни одному из предыдущих
+                    break; //Выводит только один раз
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
 
+        //Создаём определённый зоопарк:
         Zoo zoo1 = new Zoo();
-        Tiger tiger1 = new Tiger();
-        Bear bear1 = new Bear();
-        Goat goat1 = new Goat();
 
-        Animal[] zooAnimals = new Animal[3];
-        zooAnimals[0] = tiger1;
-        zooAnimals[1] = bear1;
-        zooAnimals[2] = goat1;
+//      Здесь можно изменить число мест в этом зоопарке: zoo1.zooSize = 10;
 
-//        String[] zooAnimalsNames = new String[3];
-//        zooAnimalsNames[0] = tiger1.getName();
-//        zooAnimalsNames[1] = bear1.getName();
-//        zooAnimalsNames[2] = goat1.getName();
+//        //Делаем в этом зоопарке места равными и пустыми, чтобы сработал метод takeAnimals:
+//        zoo1.createSpots(zooResidents);
 
-        zoo1.getAnimals(zooAnimals);
-//        zoo1.getAnimalNames(zooAnimalsNames);
+        //Создаём определённых зверей:
+        Tiger tiger1 = new Tiger("Tiger1", 5);
+        Bear bear1 = new Bear("Bear1", 3);
+        Goat goat1 = new Goat("Goat1", 4);
+
+        //Принимаем зверей в зоопарк и выводим их данные:
+        zoo1.takeAnimals(tiger1, bear1, goat1);
+//        zoo1.showAnimals(zooResidents);
+
+        //Создаём и принимаем следующих зверей:
+        Tiger tiger2 = new Tiger("Tiger2", 15);
+        Bear bear2 = new Bear("Bear2", 13);
+        Goat goat2 = new Goat("Goat2", 14);
+
+        //Принимаем следующих зверей в зоопарк и выводим их данные:
+        zoo1.takeAnimals(tiger2, bear2, goat2);
+        zoo1.showAnimals(zooResidents);
+        //И т. д. пока зоопарк не заполнится.
     }
 }
