7f42867b53a79fa11fd93af9d71b719077c9619f
atomashchuk <ayanovych@gmail.com>
2019-07-16 17:51:55 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_7_Inheritance_Polymorphism/Lab273_Equals/EthernetAdapter.java
@@ -0,0 +1,27 @@
+package Tomashchuk_Anna.LabWork_2_7_Inheritance_Polymorphism.Lab273_Equals;
+
+public class EthernetAdapter extends Device {
+    public int speed;
+    public String mac;
+
+    @Override
+    public String toString() {
+        return "Adapter: ";
+    }
+
+    //Add constructor
+    EthernetAdapter(int speed1, String mac1, String manufacturer, float price, String serialNumber) {
+        speed = speed1;
+        mac = mac1;
+
+        setManufacturer(manufacturer);
+        setPrice(price);
+        setSerialNumber(serialNumber);
+    }
+
+    public void showAdapter() {
+        System.out.println("Adapter: " + "speed=" + speed + ", mac=" + mac);
+    }
+
+}
+
---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_7_Inheritance_Polymorphism/Lab273_Equals/Monitor.java
@@ -0,0 +1,41 @@
+package Tomashchuk_Anna.LabWork_2_7_Inheritance_Polymorphism.Lab273_Equals;
+
+public class Monitor extends Device {
+    public int resolutionX;
+    public int resolutionY;
+
+    @Override
+    public String toString() {
+        return "Monitor: ";
+    }
+
+    //Add constructor
+    Monitor(int resX, int resY, String manufacturer, float price, String serialNumber) {
+        resolutionX = resX;
+        resolutionY = resY;
+
+        setManufacturer(manufacturer);
+        setPrice(price);
+        setSerialNumber(serialNumber);
+    }
+
+
+    public void showMonitor() {
+        System.out.println("resolutionX=" + resolutionX + ", resolutionY=" + resolutionY);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(!this.getClass().toString().equals(obj.getClass().toString())){
+            return false;
+        }
+        Monitor second=(Monitor) obj;
+        return this.manufacturer.equals(second.manufacturer) &&
+                this.resolutionX==second.resolutionX && this.resolutionY==second.resolutionY &&
+                this.price==second.price;
+
+    }
+
+
+
+}
