eb048c0399f3acb83db0e2952526e953a996660b
atomashchuk <ayanovych@gmail.com>
2019-07-18 13:46:01 +0300

    New lab

--- Module1-5/src/Tomashchuk_Anna/LabWork_2_7_Inheritance_Polymorphism/Lab273_Equals/Device.java
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_7_Inheritance_Polymorphism/Lab273_Equals/Device.java
@@ -1,15 +1,10 @@
 package Tomashchuk_Anna.LabWork_2_7_Inheritance_Polymorphism.Lab273_Equals;
 
 public class Device {
-    public String manufacturer;
-    public float price;
-    private String serialNumber;
-
-    @Override
-    public String toString(){
-        return "Device: ";
-    }
-
+    // protected для метода equals в классе Monitor, когда было private не отображалось
+    protected String manufacturer;
+    protected float price;
+    protected String serialNumber;
 
     //Add getters/setters
     public String getManufacturer(){return manufacturer;}
@@ -20,8 +15,27 @@ public class Device {
     public void setPrice(float price){this.price=price;}
     public void setSerialNumber(String serialNumber){this.serialNumber=serialNumber;}
 
-    public void showDeviceParam(){
-        System.out.println("manufacturer="+manufacturer+", price="+ price+", serialNumber="+ serialNumber);
+    //Add constructor
+    public Device(String manufacturer, float price, String serialNumber){
+        setManufacturer(manufacturer);
+        setPrice(price);
+        setSerialNumber(serialNumber);
     }
 
+    @Override //Overriding toString() method
+    public String toString() {
+        return "Device: manufacturer= " + this.getManufacturer() +
+                ", price=" + this.getPrice() + ", serialNumber= " + this.getSerialNumber();
+    }
+
+
+    @Override // Overriding equals() method
+    public boolean equals(Object obj){
+        if(!this.getClass().toString().equals(obj.getClass().toString())){
+            return false;
+        }
+        Device secondDevice=(Device) obj;
+        return this.manufacturer.equals(secondDevice.manufacturer)&&
+                this.price==secondDevice.price;
+    }
 }
