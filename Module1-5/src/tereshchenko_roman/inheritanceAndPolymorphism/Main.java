788d53c2d0544a4c2ed17c0f37928faeca88996f
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-07-23 19:49:23 +0300

    My lab works 1_5

--- Module1-5/src/tereshchenko_roman/inheritanceAndPolymorphism/Main.java
+++ Module1-5/src/tereshchenko_roman/inheritanceAndPolymorphism/Main.java
@@ -7,8 +7,8 @@ public class Main {
            Monitor monitor1 = new Monitor("Samsung", 120.25f, "S1234567", 112, 256);
            EthernetAdapter ethernetAdapter1 = new EthernetAdapter("Asus", 56.50f, "A9876543", 512, "Mac");
 
-           System.out.println(monitor1.toString());
-           System.out.println(ethernetAdapter1.toString());
+           System.out.println(monitor1);
+           System.out.println(ethernetAdapter1);
     }
 }
 
