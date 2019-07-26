72435bfaf813ee8c3701c0c07df042e39b038d2a
MykhailoMike <mkaloshyn@gmail.com>
2019-07-26 15:40:22 +0300

    my lab 2.7 upd in full

--- Module1-5/src/mykhailo_kaloshyn/mainacad/lab2_7/Main.java
+++ Module1-5/src/mykhailo_kaloshyn/mainacad/lab2_7/Main.java
@@ -4,14 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Device device1 = new Device("Samsung", 120, "AB14570911");
+        Device device2 = new Device("Samsung", 120, "AB14570911");
 
         Monitor monitor1 = new Monitor("Samsung", 100, "AB19202020", 1280, 1024);
+        Monitor monitor2 = new Monitor("Dell", 140, "AB19212024", 1340, 1100);
+        Monitor monitor3 = new Monitor("Samsung", 120, "AB19202222", 1280, 1024);
 
-        System.out.println(device1);
-        System.out.println(monitor1);
+        EthernetAdapter ethernetAdapter1 = new EthernetAdapter("HP", 210, "GR1450PK", 45, "L4012");
+        EthernetAdapter ethernetAdapter2 = new EthernetAdapter("HP", 210, "GR1450PK", 60, "L7070");
 
+        Device[] arrOfDevices = new Device[] {device1, device2, monitor1, monitor2, monitor3, ethernetAdapter1, ethernetAdapter2};
+
+        System.out.println(ethernetAdapter1.equals(ethernetAdapter2));
+        System.out.println(ethernetAdapter1.hashCode());
+        System.out.println(ethernetAdapter2.hashCode());
+        System.out.println(ethernetAdapter1);
+        System.out.println(ethernetAdapter2);
 
     }
 }
---
+++ Module1-5/src/mykhailo_kaloshyn/mainacad/testshapes/Triangle.java
@@ -0,0 +1,25 @@
+package mykhailo_kaloshyn.mainacad.testshapes;
+
+public class Triangle extends Shape {
+
+    private double a, b, c;
+
+    public Triangle(String color, double a, double b, double c) {
+        super(color);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double calcArea() {
+        double s = (a + b + c) / 2;
+        double area = Math.sqrt((s * (s - a) * (s - b) * (s - c)));
+        return area;
+    }
+
+    @Override
+    public String toString() {
+        return "This is: " + getClass() + ", color is: " + getShapeColor() + ", a = " + a + ", b = " + b + ", c = " + c;
+    }
+}
