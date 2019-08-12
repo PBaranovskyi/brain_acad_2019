217dd4fbb7f71daa8d0f1a9d54eed167c25dfeee
atomashchuk <ayanovych@gmail.com>
2019-08-12 19:12:14 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_8_Abstract_classes_Interfaces/Lab282_MyShapes/Shape.java
@@ -0,0 +1,23 @@
+package Tomashchuk_Anna.LabWork_2_8_Abstract_classes_Interfaces.Lab282_MyShapes;
+
+abstract class Shape {
+    private String shapeColor;
+    private String shapeName = "Shape";
+
+    public String getShapeColor() {
+        return shapeColor;
+    }
+
+    public Shape(String shapeColor) {
+        this.shapeColor = shapeColor;
+    }
+
+    @Override
+    public String toString() {
+
+        return "This is " + this.shapeName + ", color is: " + this.shapeColor;
+    }
+
+    abstract double calcArea();
+
+}
---
+++ Module1-5/src/Tomashchuk_Anna/LabWork_2_8_Abstract_classes_Interfaces/Lab282_MyShapes/Triangle.java
@@ -0,0 +1,35 @@
+package Tomashchuk_Anna.LabWork_2_8_Abstract_classes_Interfaces.Lab282_MyShapes;
+
+public class Triangle extends Shape {
+    private String shapeName = "Triangle";
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(String shapeColor) {
+        super(shapeColor);
+    }
+
+    public Triangle(String shapeColor, double a, double b, double c) {
+        super(shapeColor);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
+    // Triangle override calcArea() method
+    @Override
+    public double calcArea() {
+        double s = (a + b + c) / 2;
+        double x = s * (s - a) * (s - b) * (s - c);
+        double area = Math.sqrt(x);
+        return area;
+    }
+
+    @Override
+    public String toString() {
+        return "This is " + this.shapeName + ", color: " + super.getShapeColor() + ", a=" + a + ",b=" + b + ",c=" + c;
+    }
+
+}
