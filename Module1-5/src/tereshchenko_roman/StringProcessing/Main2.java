9b6a3db0dec1685532f5ca31b60b7e29539e7c9f
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-08-16 14:18:27 +0300

    My lab works 1_5

---
+++ Module1-5/src/tereshchenko_roman/StringProcessing/Main2.java
@@ -0,0 +1,23 @@
+package tereshchenko_roman.StringProcessing;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        String myStr1 = "Cartoon";
+        String myStr2 = "Tomcat";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < myStr1.length(); i++) {
+            char temp = myStr1.charAt(i);
+            for (int j = 0; j < myStr2.length(); j++) {
+                if (myStr2.indexOf(temp) == -1) {
+                    sb.append(temp);
+                    break;
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
