8cbec8eafb9a9d2609c8c0ca953fc9993d25e426
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-08-16 09:28:47 +0300

    My lab works 1_5

---
+++ Module1-5/src/tereshchenko_roman/StringProcessing/Main.java
@@ -0,0 +1,33 @@
+package tereshchenko_roman.StringProcessing;
+
+public class Main {
+
+
+    static String reverseString(String myString) {
+
+        char[] reversedChar = new char[myString.length()];
+
+        for (int i = 0; i < myString.length(); i++) {
+            reversedChar[i] = myString.charAt(myString.length()-1 - i);
+        }
+        String reversedString = new String(reversedChar);
+        return reversedString;
+    }
+
+    public static void main(String[] args) {
+
+        String myStr = "abracadabra";
+
+        int i1 = myStr.indexOf("ra");
+        System.out.println(i1);
+
+        int i2 = myStr.lastIndexOf("ra");
+        System.out.println(i2);
+
+        String subStr1 = myStr.substring(3, 7);
+        System.out.println(subStr1);
+
+        System.out.println(reverseString(myStr));
+
+    }
+}
