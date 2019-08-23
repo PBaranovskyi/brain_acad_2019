d68f62cbe11120c0c7035d9f5773905545b9a29f
Kicum <garrysinitsin@gmail.com>
2019-08-23 20:20:30 +0300

    LabWork 2.9.3 with changes from Sinitsyn Ihor.

---
+++ Module1-5/src/sinitsyn_ihor/Test.java
@@ -0,0 +1,28 @@
+package sinitsyn_ihor;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Enter month (in English with big letter first, please: ");
+        Scanner scan = new Scanner(System.in);
+        String myMonth = scan.nextLine();
+        Test.checkMonth(myMonth);
+    }
+
+    static int checkMonth(String month) {
+        int days = 0;
+        if (month == "february") {
+            days = 28;
+            System.out.println(days + " days in this month.");
+        } else if (month == "January" & month == "March" & month == "May" & month == "July" & month == "August" & month == "October" & month == "December"){
+            days = 31;
+            System.out.println(days + " days in this month.");
+        } else {
+            days = 30;
+            System.out.println(days + " days in this month.");
+        }
+        return days;
+    }
+}
