2b225954c5dd8559e256fcbd6d15f59f261abdf5
atomashchuk <ayanovych@gmail.com>
2019-08-27 19:42:16 +0300

    New lab

--- Module1-5/src/Tomashchuk_Anna/Lab2_13_The_Enums/lab2_13_1/Main.java
+++ Module1-5/src/Tomashchuk_Anna/Lab2_13_The_Enums/lab2_13_1/Main.java
@@ -2,6 +2,8 @@ package Tomashchuk_Anna.Lab2_13_The_Enums.lab2_13_1;
 
 public class Main {
     public static void main(String[] args) {
-
+     for(MyDayOfWeek day: MyDayOfWeek.values()){
+         System.out.println(day);
+     }
     }
 }
