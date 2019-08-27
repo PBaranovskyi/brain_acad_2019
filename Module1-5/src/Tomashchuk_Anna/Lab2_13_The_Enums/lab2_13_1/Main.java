d7ac8f4caf0f64bbd5e8c907cbf6ba8d73881165
atomashchuk <ayanovych@gmail.com>
2019-08-27 19:40:01 +0300

    New lab

---
+++ Module1-5/src/Tomashchuk_Anna/Lab2_13_The_Enums/lab2_13_1/Main.java
@@ -0,0 +1,7 @@
+package Tomashchuk_Anna.Lab2_13_The_Enums.lab2_13_1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Module1-5/src/Tomashchuk_Anna/Lab2_13_The_Enums/lab2_13_1/MyDayOfWeek.java
@@ -0,0 +1,11 @@
+package Tomashchuk_Anna.Lab2_13_The_Enums.lab2_13_1;
+
+public enum MyDayOfWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
