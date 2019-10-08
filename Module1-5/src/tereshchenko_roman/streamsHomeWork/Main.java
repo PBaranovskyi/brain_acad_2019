19e51d3ad9bbef60593597cdab0a50fecb6088bc
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-10-08 16:04:07 +0300

    My lab works 1_5

--- Module1-5/src/tereshchenko_roman/streamsHomeWork/Main.java
+++ Module1-5/src/tereshchenko_roman/streamsHomeWork/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
 
         List<Employee> employeeList1 = Arrays.asList(new Employee("Anna", 25), new Employee("B", 50), new Employee("C", 50));
-        List<Employee> employeeList2 = Arrays.asList(new Employee("D", 25), new Employee("E", 50), new Employee("C", 50));
-        List<Employee> employeeList3 = Arrays.asList(new Employee("F", 25), new Employee("G", 50), new Employee("C", 50));
+        List<Employee> employeeList2 = Arrays.asList(new Employee("D", 25), new Employee("E", 50), new Employee("F", 50));
+        List<Employee> employeeList3 = Arrays.asList(new Employee("G", 25), new Employee("H", 50), new Employee("I", 50));
 
         Company company1 = new Company(employeeList1);
         Company company2 = new Company(employeeList2);
@@ -25,7 +25,9 @@ public class Main {
 
         Map<Integer, List<String>> employeesMap = new HashMap<>();
 
-        Set<Integer> employeesAge = employeeSet.stream().map(employee -> employee.getAge()).collect(Collectors.toSet());
+        Set<Integer> employeesAge = employeeSet.stream()
+                .map(employee -> employee.getAge())
+                .collect(Collectors.toSet());
 
         for (Integer eachAge : employeesAge) {
             employeesMap.put(eachAge, new ArrayList<>());
@@ -39,6 +41,17 @@ public class Main {
 
 //        System.out.println(employeeSet.stream().map(p -> p.getAge()).collect(Collectors.toList()));
 
+        Map<Integer, Set<String>> employeesMap2 = companies.stream()
+                .flatMap(company -> company.getEmployeeList().stream())
+                .collect(Collectors.groupingBy(Employee::getAge, Collectors.mapping(Employee::getName, Collectors.toSet())));
+
+        System.out.println(employeesMap2);
+
+        Map<Integer, Long> employeesMap3 = companies.stream()
+                .flatMap(company -> company.getEmployeeList().stream())
+                .collect(Collectors.groupingBy(Employee::getAge, Collectors.counting()));
+
+        System.out.println(employeesMap3);
     }
 
 
