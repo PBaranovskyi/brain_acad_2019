55c8f6ef7748453dffef02190f806da283f89e7c
Pavlo Baranovskyi <baranovskiy.p.p@gmail.com>
2019-11-05 22:39:28 +0200

    java fx module 2

---
+++ javafx/src/main/java/fxclient/User.java
@@ -0,0 +1,60 @@
+package fxclient;
+
+public class User {
+
+    private String name;
+    private int age;
+    private String companyName;
+    private boolean active;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", companyName='" + companyName + '\'' +
+                '}';
+    }
+
+}
